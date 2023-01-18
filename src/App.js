4f955c2ae286cd1245952024423949eacdb936f6
liba-ny <94902436+liba-ny@users.noreply.github.com>
2023-01-18 16:19:41 +0100

    Edit more

--- src/App.js
+++ src/App.js
@@ -4,40 +4,55 @@ import React, { useState } from 'react';
 function App() {
   const [top, setTop] = useState('');
   const [bottom, setBottom] = useState('');
+  const [meme, setMeme] = useState('both');
+
   return (
     <div>
       <h1>React Meme Generator</h1>
-      <label for="top">Top text </label>
-      <input
-        id="top"
-        value={top}
-        onChange={(event) => {
-          setTop(event.currentTarget.value);
-        }}
-      />
-      <br />
-      <br />
-      <label for="bottom">Buttom text </label>
-      <input
-        id="bottom"
-        value={bottom}
-        onChange={(event) => {
-          setBottom(event.currentTarget.value);
-        }}
-      />
-      <br />
-      <br />
-      <br />
-      <h2>
-        https://api.memegen.link/images/both/{top}/{bottom}.png
-      </h2>
+      <div className="labels">
+        <label for="meme">Meme template</label>
+        <input
+          id="meme"
+          value={meme}
+          onChange={(event) => {
+            setMeme(event.currentTarget.value);
+          }}
+        ></input>
+        <label for="top">Top text </label>
+        <input
+          id="top"
+          value={top}
+          onChange={(event) => {
+            setTop(event.currentTarget.value);
+          }}
+        />
+        <br />
+        <br />
+        <label for="bottom">Bottom text </label>
+        <input
+          id="bottom"
+          value={bottom}
+          onChange={(event) => {
+            setBottom(event.currentTarget.value);
+          }}
+        />
+      </div>
       <button
+        className="btn"
         onClick={() => {
-          console.log({ top });
+          ('https://api.memegen.link/images/both/{top}/{bottom}.png');
         }}
       >
-        Preview
+        Download
       </button>
+      <br />
+      <br />
+      <br />
+
+      <img
+        src="https://api.memegen.link/images/both/{top}/{bottom}.png"
+        alt="meme"
+      ></img>
     </div>
   );
 }
