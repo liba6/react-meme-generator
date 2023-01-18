1fba7a188f94e675df2951e7e9a8dc9a173c08b9
liba-ny <94902436+liba-ny@users.noreply.github.com>
2023-01-18 10:19:47 +0100

    Change small stuff

--- src/App.js
+++ src/App.js
@@ -8,22 +8,36 @@ function App() {
     <div>
       <h1>React Meme Generator</h1>
       <label for="top">Write the top text </label>
-      <input id="top" value={setTop} />
+      <input
+        id="top"
+        value={top}
+        onChange={(event) => {
+          setTop(event.currentTarget.value);
+        }}
+      />
       <br />
       <br />
       <label for="bottom"> Write the bottom text </label>
-      <input id="bottom" value={setBottom} />
+      <input
+        id="bottom"
+        value={bottom}
+        onChange={(event) => {
+          setBottom(event.currentTarget.value);
+        }}
+      />
       <br />
       <br />
       <button
-        onClick={() => (
-          <h2>
-            'https://api.memegen.link/images/buzz/{top}/{bottom}.gif'
-          </h2>
-        )}
+        onClick={(event) => {
+          console.log(`{top} {bottom}`);
+        }}
       >
         Preview
       </button>
+      <h2>
+        {top}
+        {bottom}
+      </h2>
     </div>
   );
 }
